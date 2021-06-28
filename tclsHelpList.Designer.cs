c7498a46f403224b853d410955c000e484187cf0
MDAutomotiveProjects <matthew@mdac.com.au>
2021-06-28 13:03:43 +1000

    Committed missing help list class files.

---
+++ tclsHelpList.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace UDP
+{
+    partial class tclsHelpList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tclsHelpList));
+            this.tableLayoutPanelCharHelp = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelCharHelp
+            // 
+            this.tableLayoutPanelCharHelp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.tableLayoutPanelCharHelp.ColumnCount = 3;
+            this.tableLayoutPanelCharHelp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanelCharHelp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 45F));
+            this.tableLayoutPanelCharHelp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanelCharHelp.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelCharHelp.Location = new System.Drawing.Point(5, 5);
+            this.tableLayoutPanelCharHelp.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanelCharHelp.Name = "tableLayoutPanelCharHelp";
+            this.tableLayoutPanelCharHelp.Padding = new System.Windows.Forms.Padding(5);
+            this.tableLayoutPanelCharHelp.RowCount = 1;
+            this.tableLayoutPanelCharHelp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 440F));
+            this.tableLayoutPanelCharHelp.Size = new System.Drawing.Size(790, 440);
+            this.tableLayoutPanelCharHelp.TabIndex = 0;
+            // 
+            // tclsHelpList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanelCharHelp);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "tclsHelpList";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Text = "Help List";
+            this.Load += new System.EventHandler(this.tclsHelpList_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelCharHelp;
+    }
+}
---
+++ tclsHelpList.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDP
+{
+    public partial class tclsHelpList : Form
+    {
+        List<Button> mlstCharButtons;
+        int[] maiCharIndices;
+        public tclsHelpList(int[] charIDXList)
+        {
+            int charIDX;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(tclsHelpList));
+
+            maiCharIndices = charIDXList;
+            InitializeComponent();
+            this.tableLayoutPanelCharHelp.RowCount = charIDXList.Count();
+            mlstCharButtons = new List<Button>();
+            tableLayoutPanelCharHelp.RowStyles.Clear();
+
+
+            for (charIDX = 0; charIDX < charIDXList.Count(); charIDX++)
+            {
+                Label charLabel = new Label();
+                charLabel.BackColor = Color.FromArgb(255, 32, 32, 32);
+                charLabel.ForeColor = Color.Aquamarine;
+                charLabel.Text = tclsASAM.milstCharacteristicList[charIDXList[charIDX]].szCharacteristicName;
+                charLabel.AutoSize = true;
+                charLabel.TextAlign = ContentAlignment.BottomRight;
+                tableLayoutPanelCharHelp.Controls.Add(charLabel, 0, charIDX);
+
+                Label charLabelDetail = new Label();
+                charLabelDetail.BackColor = Color.FromArgb(255, 32, 32, 32);
+                charLabelDetail.ForeColor = Color.Aquamarine;
+
+                charLabelDetail.Text = "Min: " + tclsASAM.milstCharacteristicList[charIDXList[charIDX]].sLowerLim.ToString() +
+                                         "     Max: " + tclsASAM.milstCharacteristicList[charIDXList[charIDX]].sUpperLim.ToString();
+                charLabelDetail.AutoSize = true;
+                charLabelDetail.TextAlign = ContentAlignment.BottomRight;
+                tableLayoutPanelCharHelp.Controls.Add(charLabelDetail, 1, charIDX);
+
+                Button charHelpButton = new Button();
+                Bitmap helpImage = Properties.Resources.HELP_36;
+                charHelpButton.BackgroundImage = helpImage;
+                charHelpButton.UseVisualStyleBackColor = true;
+                charHelpButton.Width = 24;
+                charHelpButton.Height = 24;
+                charHelpButton.ImageAlign = ContentAlignment.MiddleCenter;
+                charHelpButton.BackgroundImageLayout = ImageLayout.Stretch;
+                mlstCharButtons.Add(charHelpButton);
+                charHelpButton.Click += new EventHandler(CharHelpButtonClicked);
+                tableLayoutPanelCharHelp.Controls.Add(charHelpButton, 2, charIDX);
+                tableLayoutPanelCharHelp.RowStyles.Add(new RowStyle(SizeType.AutoSize));
+            }
+
+
+            this.Height = 60 + (30 * charIDX);
+            this.Width = 550;
+
+            //this.Show();
+        }
+
+        private void CharHelpButtonClicked(object sender, EventArgs e)
+        {
+            int iControlIDX;
+            int iCharacteristicIDX;
+            bool boHTMLShown = false;
+
+            iControlIDX = mlstCharButtons.IndexOf((Button)sender);
+            iCharacteristicIDX = maiCharIndices[iControlIDX];
+
+
+            if (tclsASAM.milstCharacteristicList[iCharacteristicIDX].szInfoString.Contains("HTML"))
+            {
+                tclsHTMLHelp clsHTMLHelp;
+
+                int iLen = tclsASAM.milstCharacteristicList[iCharacteristicIDX].szInfoString.Length;
+                int iPos = tclsASAM.milstCharacteristicList[iCharacteristicIDX].szInfoString.IndexOf("HTML");
+                String szHTMLResource = tclsASAM.milstCharacteristicList[iCharacteristicIDX].szInfoString.Substring(iPos, iLen - iPos);
+
+                while (szHTMLResource.Contains(" "))
+                {
+                    szHTMLResource = szHTMLResource.Replace(" ", "");
+                }
+
+
+                szHTMLResource = szHTMLResource.Replace("HTML=", "");
+                iPos = szHTMLResource.IndexOf("HTML");
+                szHTMLResource = szHTMLResource.Substring(0, iPos) + "HTML";
+
+                String szXMLPath = AppDomain.CurrentDomain.BaseDirectory + "Database\\Help Databases\\" + szHTMLResource;
+
+                if (File.Exists(szXMLPath))
+                {
+                    clsHTMLHelp = new tclsHTMLHelp(szXMLPath);
+                    boHTMLShown = true;
+                    clsHTMLHelp.ShowDialog();
+                }
+            }
+
+
+            if (false == boHTMLShown)
+            {
+                int iPos = tclsASAM.milstCharacteristicList[maiCharIndices[0]].szInfoString.IndexOf("HTML");
+                String szShowString;
+
+                if (-1 != iPos)
+                {
+                    szShowString = tclsASAM.milstCharacteristicList[maiCharIndices[0]].szInfoString.Substring(1, iPos - 2);
+                }
+                else
+                {
+                    szShowString = tclsASAM.milstCharacteristicList[maiCharIndices[0]].szInfoString;
+                }
+
+                tclsNotify clsNotify = new UDP.tclsNotify();
+
+                clsNotify.Left = this.ClientRectangle.Width / 2;
+                clsNotify.Top = this.ClientRectangle.Height / 2;
+                clsNotify.vSetNotices("Map Help", szShowString);
+                clsNotify.ShowDialog();
+            }
+        }
+
+        private void tclsHelpList_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
